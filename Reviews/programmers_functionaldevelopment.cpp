06412da9123ce0967e76b43114b9deaab28dd735
jacob3015 <jacob3015@gmail.com>
2022-06-06 22:22:42 +0900

    Update reviews

---
+++ Reviews/programmers_functionaldevelopment.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+/**
+ * 풀리긴 풀리는데 몇몇 테스트 케이스에서 시간 초과 나옴.
+ */
+int main() {
+    
+    // 입력 vector
+    vector<int> progresses;
+    vector<int> speeds;
+
+    // 결과 vector
+    vector<int> answer;
+
+    queue<int> time;
+
+    int psize = progresses.size();
+    for(int i = 0; i < psize; i++) {
+        int diff = 100 - progresses[i];
+        if(diff % speeds[i] != 0) {
+            diff += speeds[i];
+        }
+        time.push(diff/speeds[i]);
+    }
+
+    int pre = time.front();
+    int num = 0;
+    while(!time.empty()) {
+        if(pre >= time.front()) {
+            num++;
+            time.pop();
+        }
+        else {
+            answer.push_back(num);
+            pre = time.front();
+            num = 0;
+        }
+    }
+
+    if(num != 0) {
+        answer.push_back(num);
+    }
+
+    return 0;
+}
