09788171d78143179934e1492bc82d178472ce3c
jacob3015 <jacob3015@gmail.com>
2022-06-19 17:56:17 +0900

    Update reviews

---
+++ Reviews/programmers_targetnumber.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int answer = 0;
+
+void dfs(vector<int> numbers, int target, int index, int sum) {
+    if(index == numbers.size()) {
+        if(target == sum) {
+            answer++;
+        }
+        return;
+    }
+
+    dfs(numbers, target, index + 1, sum + numbers[index]);
+    dfs(numbers, target, index + 1, sum - numbers[index]);
+}
+
+int solution(vector<int> numbers, int target) {
+    dfs(numbers, target, 0, 0);
+    return answer;
+}
